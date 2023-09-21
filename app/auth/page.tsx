420549e972c78ac99b0cee660a26a957faf5bac3
ydw1996 <didekdnlt1996@gmail.com>
2023-09-22 00:49:08 +0900

    netflix login screen

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,3 +1,5 @@
+import Input from "@/components/input";
+
 const Auth = () => {
     return (
         <div className="relative h-full w-full bg-[url('/image/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -5,6 +7,16 @@ const Auth = () => {
                 <nav className="px-12 py-5">
                     <img src="image/logo.png" alt="logo" className="h-12"/>
                 </nav>
+                <div className="flex justify-center">
+                    <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rouned-md w-full">
+                        <h2 className="text-white text-4xl mb-8 font-semibold">
+                            Sign in
+                        </h2>
+                        <div className="flex flex-col gap-2">
+                            <Input/>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
