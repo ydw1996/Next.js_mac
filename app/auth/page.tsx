a3d5942e7efe93f9cf2ff0f29e40517ef5ee569b
ydw1996 <didekdnlt1996@gmail.com>
2023-09-25 00:15:36 +0900

    input usestate event

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,10 +1,12 @@
+'use client';
 import Input from "@/components/input";
-import { useState } from "react";
+import React, { useState } from 'react';
+
 
 const Auth = () => {
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
-
+    const [password, setPassword] = useState('');
     return (
         <div className="relative h-full w-full bg-[url('/image/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
             <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -18,17 +20,25 @@ const Auth = () => {
                         </h2>
                         <div className="flex flex-col gap-2">
                             <Input
-                            label="User name"
-                            onchange={(ev) => setName(ev.target.value)}
+                            label="Username"
+                            onchange={(ev: any) => setName(ev.target.value)}
                             id="name"
+                            type="name"
                             value={name}
                             />
                             <Input
                             label="Email"
-                            onchange={(ev) => setEmail(ev.target.value)}
+                            onchange={(ev: any) => setEmail(ev.target.value)}
                             id="email"
                             type="email"
                             value={email}
+                            />
+                            <Input
+                            label="Password"
+                            onchange={(ev: any) => setPassword(ev.target.value)}
+                            id="password"
+                            type="password"
+                            value={password}
                             />
                         </div>
                     </div>
